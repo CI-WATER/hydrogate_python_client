c8643ada1f1c629d4dd429e6e416c3c5ff2bfcb4
Pabitra Dash <pkdash_reena@hotmail.com>
2015-06-16 12:07:14 -0600

    Added test for concatenate netcdf files api

---
+++ tests/non_hpc/concatenate_netcdf_tests.py
@@ -0,0 +1,37 @@
+__author__ = 'Pabitra'
+
+from hydrogate import HydroDS
+import settings
+
+hds = HydroDS(username=settings.USER_NAME, password=settings.PASSWORD)
+
+# NOTE: this file path is valid for user with id 2
+ref_input_raster_url = 'http://hydro-ds.uwrl.usu.edu:20199/files/data/user_2/SpawnProj.tif'
+input_netcdf_static_file_1 = 'prcp_2010.nc4'     # this a static dayamet data file on the api server
+input_netcdf_static_file_2 = 'prcp_2011.nc4'     # this a static dayamet data file on the api server
+
+try:
+    # generate 2 netcdf files that can be concatenated
+    response_data = hds.subset_netcdf(input_netcdf=input_netcdf_static_file_1, ref_raster_url_path=ref_input_raster_url,
+                                      output_netcdf='prcp_2010_spawn.nc')
+    prcp_2010_subset_netcdf_url = response_data['output_netcdf']
+    print(prcp_2010_subset_netcdf_url)
+
+    response_data = hds.subset_netcdf(input_netcdf=input_netcdf_static_file_2, ref_raster_url_path=ref_input_raster_url,
+                                      output_netcdf='prcp_2011_spawn.nc')
+    prcp_2011_subset_netcdf_url = response_data['output_netcdf']
+    print(prcp_2011_subset_netcdf_url)
+
+    # concatenate the above 2 output netcdf files
+    # param: output_netcdf is optional
+    response_data = hds.concatenate_netcdf(input_netcdf1_url_path=prcp_2010_subset_netcdf_url,
+                                           input_netcdf2_url_path=prcp_2011_subset_netcdf_url,
+                                           output_netcdf='prcp_2010_2011_spawn.nc')
+    output_concatenated_netcdf_url = response_data['output_netcdf']
+    print(output_concatenated_netcdf_url)
+except Exception as ex:
+    print(ex.message)
+
+print ">>>> DONE..."
+exit()
+
